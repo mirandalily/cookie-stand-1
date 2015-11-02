27ebca4f7fa0847f833339385352910752d8fe21
Ivar Lee <ivarr5@gmail.com>
2015-11-02 12:56:01 -0800

    pushing changes to branch

---
+++ app.js
@@ -0,0 +1,31 @@
+// Pike Place
+var pikePlaceMarket = {
+  minCustHr: 17,
+  maxCustHr: 88,
+  avgCookiesCust: 5.2,
+  calcRand: function() {
+    Math.floor(Math.random() * (88 - 17 + 1)) + 17;
+  };
+  //change this to a comma when adding more properties
+  //in to this object
+}
+
+
+
+//   calcHourlyT function(),
+//   calcDailyT function(),
+//   display function()
+//   pikePlaceHourlyT=[];
+//   pikePlaceDailyT=0;
+
+
+
+
+
+// SeaTac
+
+// Southcenter Mall
+
+// Bellevue Mall
+
+// Alki
