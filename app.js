f603103a2e2ca938a84287557c39f6642b948b77
Ivar Lee <ivarr5@gmail.com>
2015-11-02 15:31:52 -0800

    added append child function

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
-// Pike Place
+var hoursArray = ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM'];
+//var location = ['pikePlaceMarket', 'seaTacAirport', 'southCenterMall', 'bellevueSquare', 'alki']
 
 var pikePlaceMarket = {
   minCustHr: 17,
   maxCustHr: 88,
   avgCookiesCust: 5.2,
+  hourlyTArray: [],
+
   calcRand: function() {
     return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
-  }
+  },
   calcHourlyT: function() {
-
-  }
-
+    return Math.floor(this.calcRand() * this.avgCookiesCust);
+  },
 };
 
-//calcHourlyT function() {
+  for (var i = 0; i < hoursArray.length; i++) {
+    pikePlaceMarket.hourlyTArray.push(pikePlaceMarket.calcHourlyT())
+}
+  for (var i = 0; i < pikePlaceMarket.hourlyTArray.length; i++) {
+    var node =document.createElement("LI");
+    var textnode =document.createTextNode(hoursArray[i] + ": " + pikePlaceMarket.hourlyTArray[i]);
+    node.appendChild(textnode);
+    document.getElementById('list').appendChild(node);
 
-//   calcDailyT function(),
-//   display function()
-//   pikePlaceHourlyT=[];
-//   pikePlaceDailyT=0;
+}
+
+
+  console.log(pikePlaceMarket.calcRand(pikePlaceMarket.minCustHr,pikePlaceMarket.maxCustHr))
+
+
+
 
 // document.getElementById('10AM')
 // document.getElementById('11AM')
