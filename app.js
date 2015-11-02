66d1b61637a1d132de1d6b22e5cd51f2d808539c
Ivar Lee <ivarr5@gmail.com>
2015-11-02 13:44:15 -0800

    got pike place market object partially set up

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 // Pike Place
+
 var pikePlaceMarket = {
   minCustHr: 17,
   maxCustHr: 88,
   avgCookiesCust: 5.2,
   calcRand: function() {
-    Math.floor(Math.random() * (88 - 17 + 1)) + 17;
-  };
-  //change this to a comma when adding more properties
-  //in to this object
-}
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
+  }
+  calcHourlyT: function() {
 
+  }
 
+};
 
-//   calcHourlyT function(),
+//calcHourlyT function() {
+
 //   calcDailyT function(),
 //   display function()
 //   pikePlaceHourlyT=[];
 //   pikePlaceDailyT=0;
+
+// document.getElementById('10AM')
+// document.getElementById('11AM')
+// document.getElementById('12PM')
+// document.getElementById('1PM')
+// document.getElementById('2PM')
+// document.getElementById('3PM')
+// document.getElementById('4PM')
+// document.getElementById('5PM')
+
+
+// document.getElementById('TOTAL')
 
 
 
