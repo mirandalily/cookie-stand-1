5b8d3e277a0bd56da8c161f7204e3e468bd81c9f
Ivar Lee <ivarr5@gmail.com>
2015-11-03 09:57:23 -0800

    more comments

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var hoursArray = ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
 
 
-////////////////////////////////////////////////////////////
+//////////////////////////PROPERTIES?///////////////////////////////////////////////////
 var pikePlaceMarket = {
   storeLocation: "Pike Place Market",
   minCustHr: 17,
@@ -11,6 +11,8 @@ var pikePlaceMarket = {
   avgCookiesCust: 5.2,
   hourlyTArray: [],
   dailyTotal: 0,
+
+/////////////////////////////METHODS/////////////////////////////////////////////////////
 
   calcRand: function() {   ///randomly generates customers per hour
     return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
@@ -20,7 +22,7 @@ var pikePlaceMarket = {
     return Math.floor(this.calcRand() * this.avgCookiesCust);
     var singleHour = this.calcHourlyT();
     this.calcHourlyT.push(singleHour);
-    this.dailyTotal += hourlyTArray[i];//this.dailyTotal += singleHour;
+    this.dailyTotal += hourlyTArray[i];//this.dailyTotal += singleHour;  ///my daily total doesn't work
   },
 
   calcCookiesHr: function() {  ///for loop that runs as long as the hours array, pushes hourly total
@@ -29,31 +31,35 @@ var pikePlaceMarket = {
     }
   },
 
+
+
+  //////////////////////////////DOM POPULATION//////////////////////////////////////////
+
   display: function() {      //populates a list in to the DOM
     for (var i = 0; i < pikePlaceMarket.hourlyTArray.length; i++) {
-      var node =document.createElement("LI");
-      var textnode =document.createTextNode(hoursArray[i] + ": " + pikePlaceMarket.hourlyTArray[i]);
-      node.appendChild(textnode);
-      document.getElementById('list').appendChild(node);
+      var node =document.createElement("LI");                                                         // Create a <li> node
+      var textnode =document.createTextNode(hoursArray[i] + ": " + pikePlaceMarket.hourlyTArray[i]);  // Create a text node
+      node.appendChild(textnode);                                                                     // Append the text to <li>
+      document.getElementById('list').appendChild(node);                                        // Append <li> to <ul> with id="myList"
     };
 
-    var node =document.createElement("LI");
-    var textnode =document.createTextNode("Total" + ": " + this.dailyTotal);
-    node.appendChild(textnode);
-    document.getElementById('list').appendChild(node);
+    var node =document.createElement("LI");                                   // Create a <li> node
+    var textnode =document.createTextNode("Total" + ": " + this.dailyTotal);  // Create a text node
+    node.appendChild(textnode);                                               // Append the text to <li>
+    document.getElementById('list').appendChild(node);            // Append <li> to <ul> with id="myList"
   }
 
 
 };
 
 //////////////////////////////////////////////////////////////
-pikePlaceMarket.calcCookiesHr();   //variabls declared outside of object
-pikePlaceMarket.display();        //variables again
+pikePlaceMarket.calcCookiesHr();   //this is called outside the object
+pikePlaceMarket.display();        //this is called outside the object
 
 
 
 
-console.log(pikePlaceMarket.calcRand(pikePlaceMarket.minCustHr,pikePlaceMarket.maxCustHr));
+console.log(pikePlaceMarket.calcRand(pikePlaceMarket.minCustHr,pikePlaceMarket.maxCustHr)); //debug
 
 
 
