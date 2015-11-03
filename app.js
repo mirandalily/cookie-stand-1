cee2c763c143888234b32c2218241202663e924e
Ivar Lee <ivarr5@gmail.com>
2015-11-03 11:55:51 -0800

    nothing

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var hoursArray = ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-//var location = ['pikePlaceMarket', 'seaTacAirport', 'southCenterMall', 'bellevueSquare', 'alki']
 
-
-
-//////////////////////////PROPERTIES?///////////////////////////////////////////////////
+////////Properties
 var pikePlaceMarket = {
   storeLocation: "Pike Place Market",
   minCustHr: 17,
@@ -12,54 +9,65 @@ var pikePlaceMarket = {
   hourlyTArray: [],
   dailyTotal: 0,
 
-/////////////////////////////METHODS/////////////////////////////////////////////////////
+/////////Methods
 
-  calcRand: function() {   ///randomly generates customers per hour
+  calcRand: function() {   ///randomly generates amount of customers
     return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1)) + this.minCustHr;
   },
 
-  calcHourlyT: function() {  ///calculating number of customers * avg cookies per customer
-    return Math.floor(this.calcRand() * this.avgCookiesCust);
-    var singleHour = this.calcHourlyT();
-    this.calcHourlyT.push(singleHour);
-    this.dailyTotal += hourlyTArray[i];//this.dailyTotal += singleHour;  ///my daily total doesn't work
-  },
-
-  calcCookiesHr: function() {  ///for loop that runs as long as the hours array, pushes hourly total
+  calcCookiesHr: function() {  ///for loop that runs as long as the hours array, pushes to hourly total array
   for (var i = 0; i < hoursArray.length; i++) {
-    pikePlaceMarket.hourlyTArray.push(pikePlaceMarket.calcHourlyT())
+    pikePlaceMarket.hourlyTArray.push(pikePlaceMarket.calcHourlyT()) //this.hourlyTArray.push(calcRand() * avgCookiesCust)
     }
   },
 
+  calcHourlyT: function() {  ///calculates random number of customers * avg cookies per customer
+    return Math.floor(this.calcRand() * this.avgCookiesCust);
+
+    var singleHour = this.calcHourlyT();  ///not sure what is happening here <------------------------
+    this.calcHourlyT.push(hourlyTArray());    ///not sure what is happening here <------------------------
+    this.dailyTotal += hourlyTArray[i];//this.dailyTotal += singleHour;  ///my daily total doesn't work <------------------------
+  },
 
 
-  //////////////////////////////DOM POPULATION//////////////////////////////////////////
+
+//////DOM POPULATION
 
   display: function() {      //populates a list in to the DOM
-    for (var i = 0; i < pikePlaceMarket.hourlyTArray.length; i++) {
+    for (var i = 0; i < pikePlaceMarket.hourlyTArray.length; i++) {               //http://www.w3schools.com/jsref/met_node_appendchild.asp
       var node =document.createElement("LI");                                                         // Create a <li> node
       var textnode =document.createTextNode(hoursArray[i] + ": " + pikePlaceMarket.hourlyTArray[i]);  // Create a text node
       node.appendChild(textnode);                                                                     // Append the text to <li>
-      document.getElementById('list').appendChild(node);                                        // Append <li> to <ul> with id="myList"
+      document.getElementById('pikelist').appendChild(node);                                        // Append <li> to <ul> with id="pikelist"
     };
-
-    var node =document.createElement("LI");                                   // Create a <li> node
-    var textnode =document.createTextNode("Total" + ": " + this.dailyTotal);  // Create a text node
-    node.appendChild(textnode);                                               // Append the text to <li>
-    document.getElementById('list').appendChild(node);            // Append <li> to <ul> with id="myList"
+                            //supposed to populate the totaly to the DOM      //http://www.w3schools.com/jsref/met_node_appendchild.asp
+      var node =document.createElement("LI");                                   // Create a <li> node
+      var textnode =document.createTextNode("Total" + ": " + this.dailyTotal);  // Create a text node
+      node.appendChild(textnode);                                               // Append the text to <li>
+      document.getElementById('pikelist').appendChild(node);            // Append <li> to <ul> with id="pikelist"
   }
-
-
 };
 
-//////////////////////////////////////////////////////////////
-pikePlaceMarket.calcCookiesHr();   //this is called outside the object
-pikePlaceMarket.display();        //this is called outside the object
+  pikePlaceMarket.calcCookiesHr();   //this is called outside the object
+  pikePlaceMarket.display();        //this is called outside the object
 
 
 
 
-console.log(pikePlaceMarket.calcRand(pikePlaceMarket.minCustHr,pikePlaceMarket.maxCustHr)); //debug
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
